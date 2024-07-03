7d3436b26f76c2dbb4a1e081045a4f695797e37a
stroligo <gabrielstroligo@gmail.com>
2024-07-03 21:04:56 +0200

    tailwind base

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./**/*.html'],
+  darkMode: false, // ou 'media' ou 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
